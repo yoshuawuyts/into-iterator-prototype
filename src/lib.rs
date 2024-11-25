dbd171ddae7bba3e5061dee052a5dea31b430d7d
Yosh <github@yosh.is>
2024-11-25 01:14:22 +0100

    init map

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 //! Experiment with methods on IntoIterator
 //!
+//! This project asks the question: what if we used `IntoIterator` everywhere
+//! instead of `Iterator`?
+//!
 //! # Examples
 //!
 //! ```
@@ -9,3 +12,40 @@
 #![forbid(unsafe_code, rust_2018_idioms)]
 #![deny(missing_debug_implementations, nonstandard_style)]
 #![warn(missing_docs, future_incompatible, unreachable_pub)]
+
+/// A trait for dealing with iterators.
+pub trait Iterator {
+    /// The type of the elements being iterated over.
+    type Item;
+
+    /// Advances the iterator and returns the next value.
+    fn next(&mut self) -> Option<Self::Item>;
+
+    /// How many items do we expect to yield?
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (0, None)
+    }
+}
+
+/// Conversion into an Iterator.
+pub trait IntoIterator {
+    /// The type of the elements being iterated over.
+    type Item;
+
+    /// Which kind of iterator are we turning this into?
+    type IntoIterator: Iterator<Item = Self::Item>;
+
+    /// Creates an iterator from a value.
+    fn into_iter(self) -> Self::IntoIterator;
+
+    /// Maps the values of iter with f.
+    fn map<F, B>(self, f: F) -> map::IntoMap<Self, F>
+    where
+        F: FnOnce(Self::Item) -> B,
+        Self: Sized,
+    {
+        map::IntoMap::new(self, f)
+    }
+}
+
+pub mod map;
---
+++ src/map.rs
@@ -0,0 +1,53 @@
+//! Helper types for the `map` operation
+use super::{IntoIterator, Iterator};
+
+/// An iterator which maps items from one type to another
+#[derive(Debug)]
+pub struct Map<I, F> {
+    pub(crate) iter: I,
+    f: F,
+}
+
+impl<I, F> Map<I, F> {
+    fn new(iter: I, f: F) -> Map<I, F> {
+        Map { iter, f }
+    }
+}
+
+/// A type that can be converted into a map iterator.
+#[derive(Debug)]
+pub struct IntoMap<I, F> {
+    iter: I,
+    f: F,
+}
+
+impl<I, F> IntoMap<I, F> {
+    pub(crate) fn new(iter: I, f: F) -> Self {
+        Self { iter, f }
+    }
+}
+
+impl<B, I: Iterator, F> Iterator for Map<I, F>
+where
+    F: FnMut(I::Item) -> B,
+{
+    type Item = B;
+
+    #[inline]
+    fn next(&mut self) -> Option<B> {
+        self.iter.next().map(&mut self.f)
+    }
+}
+
+impl<B, I: IntoIterator, F> IntoIterator for IntoMap<I, F>
+where
+    F: FnMut(I::Item) -> B,
+{
+    type Item = B;
+
+    type IntoIterator = Map<I::IntoIterator, F>;
+
+    fn into_iter(self) -> Self::IntoIterator {
+        Map::new(self.iter.into_iter(), self.f)
+    }
+}
